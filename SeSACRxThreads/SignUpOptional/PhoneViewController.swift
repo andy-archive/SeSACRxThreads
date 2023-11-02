9c1cd1dc9c412050ce84bd2195bb471cb074d5fc
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-02 19:29:55 +0900

    feat: 전화번호 유효성 검사 기능 구현 (Rx - MVC)

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -4,34 +4,79 @@
 //
 //  Created by jack on 2023/10/30.
 //
- 
-import UIKit
-import SnapKit
 
-class PhoneViewController: UIViewController {
-   
-    let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
-    let nextButton = PointButton(title: "다음")
+import UIKit
+
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class PhoneViewController: UIViewController {
+    
+    private let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
+    private let nextButton = PointButton(title: "다음")
+    
+    private let buttonEnabled = BehaviorSubject(value: false)
+    private let buttonColor = BehaviorSubject(value: UIColor.systemRed)
+    private let phoneNumber = BehaviorSubject(value: "010")
+    
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = Color.white
         
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
     }
     
-    @objc func nextButtonClicked() {
+    private func bind() {
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
+            .disposed(by: disposeBag)
+        
+        buttonColor
+            .map { $0.cgColor }
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+        
+        phoneNumber
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneNumber
+            .map { $0.count > 12 }
+            .subscribe(with: self, onNext: { owner, value in
+                let color = value ? UIColor.systemGreen : UIColor.systemRed
+                owner.buttonColor.onNext(color)
+                owner.buttonEnabled.onNext(value)
+            })
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .subscribe { value in
+                let formatted = value.formated(by: "###-####-####")
+                self.phoneNumber.onNext(formatted)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    @objc private func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
     }
-
     
-    func configureLayout() {
+    private func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
-         
+        
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
@@ -44,5 +89,4 @@ class PhoneViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
-
 }
